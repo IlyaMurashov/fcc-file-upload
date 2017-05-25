d13fb38d8c3f5bcebe5dcbdfffac06461d92d7c4
IlyaMurashov <murashov.ilya@gmail.com>
2017-05-25 16:42:54 +0300

    Ready for Heroku

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const path = require('path');
+const multer = require('multer');
+
+const app = express();
+const upload = multer();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({
+    file: req.file.originalname,
+    size: req.file.size
+  })
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
